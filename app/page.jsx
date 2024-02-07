fc3dd9c9cf3eece9caf81e9fcd897d11932aeb66
Kartik123-creator <kartikbosmiya2003@gmail.com>
2024-02-07 17:21:40 +0530

    Generated Project
    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex">
+        <div className="text-[45px] italic font-[900] leading-[45px] me-[7px]"> SUGAR </div>
+        <div className="text-[45px] font-[400] leading-[45px]"> ELITE </div>
+      </div>
+    </>
+  );
+}
