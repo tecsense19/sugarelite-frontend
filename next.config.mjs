5212444f1557bb487c0d23922f468c3f6fd5af1f
Kartik123-creator <tecsense19@gmail.com>
2024-02-20 13:30:36 +0530

    main branch updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.BASE_ROUTE,
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  async rewrites() {
-    return [
-      { source: '/admin/:path*', destination: '/admin/:path*' },
-    ];
-  },
   // trailingSlash: true,
 };
 
