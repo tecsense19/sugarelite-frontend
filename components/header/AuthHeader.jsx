c9bd88ff6ab8caea3df283b843573c9f63a6c4cb
Kartik123-creator <kartikbosmiya2003@gmail.com>
2024-02-20 11:18:20 +0530

    Bug: for horizontal scroll

--- components/header/AuthHeader.jsx
+++ components/header/AuthHeader.jsx
@@ -26,7 +26,7 @@ const AuthHeader = () => {
     <>
       {/* Mobile View */}
       {pathname === client_routes.home &&
-        <header className="md:hidden top-0 fixed w-full z-[2]" data-aos="fade-down" data-aos-duration="500">
+        <header className="md:hidden top-0 fixed w-dvw z-[2]" data-aos="fade-down" data-aos-duration="500">
           <div className="flex bg-black text-white items-center justify-center px-4">
             <div className="w-full sm:w-11/12 xl:w-9/12">
               <div className="flex justify-between items-center h-[65px]">
