f53e6582920232d94b46774099b2ca6045c06a7f
Kartik123-creator <tecsense19@gmail.com>
2024-02-20 19:02:27 +0530

    Chat page desktop completed

--- components/chat/ChatContent.jsx
+++ components/chat/ChatContent.jsx
@@ -6,12 +6,12 @@ import ChatSection from "./ChatSection";
 const ChatContent = ({ selectedObj, profiles }) => {
 
   return (
-    <div className="w-[calc(100%-400px)] h-full flex flex-col">
+    <div className="hidden md:flex md:w-[calc(100%-350px)] lg:w-[calc(100%-400px)] h-full flex-col">
       {selectedObj
         ? <>
           <ChatSection selectedObj={selectedObj} profiles={profiles} />
         </>
-        : <div className="flex justify-center items-center w-full h-full">
+        : <div className="flex justify-center items-center w-full h-full" data-aos="fade-left" data-aos-duration="800">
           <div className="text-[22px]">Please click on a profile to chat</div>
         </div>
       }
