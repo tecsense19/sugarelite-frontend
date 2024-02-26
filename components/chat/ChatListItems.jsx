55bb3913653ee1140d48acfe4e9c72e0a3465dd5
Kartik123-creator <tecsense19@gmail.com>
2024-02-26 18:43:11 +0530

    Update ChatListItems.jsx

--- components/chat/ChatListItems.jsx
+++ components/chat/ChatListItems.jsx
@@ -6,7 +6,7 @@ const ChatListItems = ({ profiles, setSelectedObj, showMobileChatContent, setSho
       <div className="mt-5 px-5 md:mt-[30px] md:px-[30px]">
         <div className="text-[20px] md:text-[26px] font-semibold md:font-bold leading-[30px]">My Chat List</div>
       </div>
-      <div className="flex md:hidden flex-col mt-[10px] md:mt-5 gap-y-[10px] md:gap-y-4 overflow-y-auto h-[calc(100%-190px)] md:h-[calc(100%-170px)] ps-4 px- md:ps-[30px] me-0 pe-4 md:me-3 md:pe-3 md:second-child" style={{ scrollbarWidth: "none" }}>
+      <div className="flex md:hidden flex-col mt-[10px] md:mt-5 gap-y-[10px] md:gap-y-4 overflow-y-auto h-[calc(100%-190px)] md:h-[calc(100%-170px)] ps-4 px- md:ps-[30px] me-0 pe-4 md:me-3 md:pe-3 second-child" style={{ scrollbarWidth: "none" }}>
         {profiles?.map((item, idx) => {
           return (
             <ChatItem
