5f13708dc0eedc76034af2c2e2303195db735c1f
Rajesh <tecsense19@gmail.com>
2024-03-08 16:27:20 +0530

    socket commented

--- components/chat/ChatContent.jsx
+++ components/chat/ChatContent.jsx
@@ -3,9 +3,9 @@
 import { useEffect, useState } from "react";
 import "./ChatContent.css";
 import ChatSection from "./ChatSection";
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
-let socket;
+// let socket;
 
 const ChatContent = ({ selectedObj, profiles, showMobileChatContent, setShowMobileChatContent, currentUser, messages }) => {
 
@@ -16,36 +16,36 @@ const ChatContent = ({ selectedObj, profiles, showMobileChatContent, setShowMobi
   }, [messages])
 
 
-  function socketInitializer() {
-    //   // fetch("/api/socket");
+  // function socketInitializer() {
+  //   //   // fetch("/api/socket");
 
-    socket = io("http://localhost:8080");
+  //   socket = io("http://localhost:8080");
 
-    socket.on("receive-message", async (data) => {
-      if (data.sender_id === currentUser.id) {
-        setChats((pre) => [...pre, data]);
-      }
-      if ((data.receiver_id === currentUser.id)) {
-        if (data.sender_id === selectedObj.id) {
-          setChats((pre) => [...pre, data]);
+  //   socket.on("receive-message", async (data) => {
+  //     if (data.sender_id === currentUser.id) {
+  //       setChats((pre) => [...pre, data]);
+  //     }
+  //     if ((data.receiver_id === currentUser.id)) {
+  //       if (data.sender_id === selectedObj.id) {
+  //         setChats((pre) => [...pre, data]);
 
-        } else {
-          // toast.info("Message from " + data.receiverName)
-        }
-      }
-    });
-  }
+  //       } else {
+  //         // toast.info("Message from " + data.receiverName)
+  //       }
+  //     }
+  //   });
+  // }
 
 
-  useEffect(() => {
-    socketInitializer()
+  // useEffect(() => {
+  //   socketInitializer()
 
-    return () => {
-      if (socket) {
-        socket.disconnect();
-      }
-    };
-  }, [])
+  //   return () => {
+  //     if (socket) {
+  //       socket.disconnect();
+  //     }
+  //   };
+  // }, [])
 
   return (
     <div className="flex w-full md:w-[calc(100%-350px)] lg:w-[calc(100%-400px)] h-full flex-col">
