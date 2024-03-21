61ccc13d02026bef60bb6a6d3ba3a6dddf6a7950
Rajesh <tecsense19@gmail.com>
2024-03-21 19:51:48 +0530

    emoji added

--- components/msg/ChatComponent/MessageInput.jsx
+++ components/msg/ChatComponent/MessageInput.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import smileIcon from "/public/assets/smile_icon.svg";
 import attachmentIcon from "/public/assets/attachment_icon.svg";
@@ -7,11 +7,16 @@ import Image from 'next/image';
 import { send_message_action } from '@/app/lib/actions';
 import { client_notification } from '@/app/lib/helpers';
 import { notification } from 'antd';
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
 
 const MessageInput = ({ socket, toUser, currentUser }) => {
 
-    const { register, handleSubmit, reset, watch, } = useForm()
+    const { register, handleSubmit, reset, watch, setValue } = useForm()
     const [api, contextHolder] = notification.useNotification()
+    const [isEmoji, setIsEmoji] = useState(false)
+    const [emoji, setEmoji] = useState('')
+    const [isClicked, setIsClicked] = useState(false)
 
     const sendMessageHandler = async ({ message }) => {
         if (message.length && currentUser && toUser) {
@@ -35,11 +40,21 @@ const MessageInput = ({ socket, toUser, currentUser }) => {
         }
     }, [watch("message")])
 
+    const emojiSelector = (emoji) => {
+        setValue('message', watch("message") + emoji.native);
+    }
+
+    const submitForm = (data) => {
+        if (data.message && isClicked) {
+            sendMessageHandler(data);
+        }
+    }
+
     return (
         <div className="w-full flex px-4 pb-[18px] md:px-10 md:pb-10 relative">
             {contextHolder}
-            <form onSubmit={handleSubmit(sendMessageHandler)} className="w-full h-12 relative md:h-[70px] rounded-[5px] bg-black flex items-center px-3 md:px-[30px]">
-                <button>
+            <form onSubmit={handleSubmit(submitForm)} className="w-full h-12 md:h-[70px] rounded-[5px] bg-black flex items-center px-3 md:px-[30px] relative">
+                <button onClick={() => setIsEmoji((prev) => !prev)}>
                     <Image src={smileIcon} priority alt="" height={28} width={28} className="hidden md:block pointer-events-none" />
                     <Image src={smileIcon} priority alt="" height={20} width={20} className="md:hidden pointer-events-none" />
                 </button>
@@ -48,15 +63,18 @@ const MessageInput = ({ socket, toUser, currentUser }) => {
                     <Image src={attachmentIcon} priority alt="" height={28} width={28} className="hidden md:block pointer-events-none" />
                     <Image src={attachmentIcon} priority alt="" height={20} width={20} className="md:hidden pointer-events-none" />
                 </button>
-                <button type="submit" className="h-[30px] w-[30px] md:h-[35px] md:w-[35px] bg-secondary flex justify-center items-center ms-3 md:ms-[30px] rounded-full" >
+                <button type="submit" onClick={() => { setIsClicked(true); setIsEmoji(false) }} className="h-[30px] w-[30px] md:h-[35px] md:w-[35px] bg-secondary flex justify-center items-center ms-3 md:ms-[30px] rounded-full" >
                     <Image src={sendIcon} priority alt="" height={18} width={18} className="hidden md:block pointer-events-none" />
                     <Image src={sendIcon} priority alt="" height={15.5} width={15.5} className="md:hidden pointer-events-none" />
                 </button>
+                <div className={`absolute hidden md:block bottom-[55px] left-4 origin-bottom ease-linear transition-transform duration-200 ${isEmoji ? "scale-y-1" : " scale-y-0"}`}>
+                    <Picker data={data} onEmojiSelect={emojiSelector} theme={"dark"} previewPosition={"none"} searchPosition="none" perLine={10} />
+                </div>
             </form>
             {/* <>
             <div className="w-full flex px-4 pb-[18px] md:px-10 md:pb-10">
-              <div className="w-full py-2 md:py-4 rounded-[5px] bg-black my-auto h-full text-center items-center px-3 md:px-[30px] text-[14px] md:text-[16px] text-white/80 justify-center">Chat limit exceeded for today! Upgrade to <Link href={client_routes.subscription} className="text-secondary inline">premium</Link> for unlimited chatting. 🚀</div>
-            </div>
+                    <div className="w-full py-2 md:py-4 rounded-[5px] bg-black my-auto h-full text-center items-center px-3 md:px-[30px] text-[14px] md:text-[16px] text-white/80 justify-center">Chat limit exceeded for today! Upgrade to <Link href={client_routes.subscription} className="text-secondary inline">premium</Link> for unlimited chatting. 🚀</div>
+                </div>
             </> */}
         </div>
 
