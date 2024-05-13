d863cf2aef70ec3f5d8baf747bfa6b0667270daa
Tec-sense <tecsense19@gmail.com>
2024-05-13 19:48:16 +0530

    app.js updated

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const next = require('next')
+const { createServer } = require("node:http")
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const httpServer = createServer(handler);
+
+
+    httpServer
+        .once("error", (err) => {
+            console.error(err);
+            process.exit(1);
+        })
+        .listen(port, () => {
+            console.log(`> Ready on http://${hostname}:${port}`);
+        });
+});
